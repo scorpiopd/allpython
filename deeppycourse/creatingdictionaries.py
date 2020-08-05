2dcdfce0072e8666c8de4b64197529bec0891d65
scorpiopd <pranavdhumal65@gmail.com>
2020-08-05 12:08:50 +0530

    adding two files

--- deeppycourse/creatingdictionaries.py
+++ deeppycourse/creatingdictionaries.py
@@ -17,3 +17,21 @@ more hashes--> slower dictionaries
 
 d={i: i ** 2 for i in range (1,5)}
 print(d)
+print(hash((1,2,3)))
+d={(1,2,3):"this is tuple" }
+
+t1=(1,2,3)
+t2=(1,2,3)
+
+print(hash(t1)==hash(t2))
+#list are not hashable
+
+def myfunc(a,b):
+    return a*b
+print(hash(myfunc(1,2)))
+
+d=dict(x=100,a=200)
+
+d={"a":100}
+print(d)
+print(id(d))

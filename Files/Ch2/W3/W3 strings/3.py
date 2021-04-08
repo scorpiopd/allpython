d78acdac369ed7310e4bbc4c2a6c0736d2ceb06b
Pranav Dhumal <46155672+scorpiopd@users.noreply.github.com>
2021-04-08 10:02:19 +0530

    Create 3.py

---
+++ Files/Ch2/W3/W3 strings/3.py
@@ -0,0 +1,10 @@
+ss="ww"
+if len(ss) < 2:
+    print("")
+else:
+    print(ss[0:2],end="")
+    print(ss[-2:])
+    
+    
+#https://stackoverflow.com/questions/509211/understanding-slice-notation
+#https://www.tutorialspoint.com/How-can-we-combine-multiple-print-statements-per-line-in-Python

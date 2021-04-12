2fe85404b1c874ac9042dfd9282e2c0e3d09a1f3
Pranav Dhumal <46155672+scorpiopd@users.noreply.github.com>
2021-04-12 13:17:40 +0530

    Create 9.py

---
+++ Files/Ch2/W3/W3 strings/9.py
@@ -0,0 +1,29 @@
+#9. Write a Python program to remove the nth index character from a nonempty string. 
+
+
+#mysol 
+
+i=int(input("enter index to remove"))
+
+a="Python"
+b=list(a)
+str1=""
+b.pop(i)
+print(str1.join(b))
+
+#without declaring variable
+#print("".join(b))
+
+
+
+
+
+
+
+
+
+
+
+
+https://stackoverflow.com/questions/5618878/how-to-convert-list-to-string
+https://www.geeksforgeeks.org/python-program-to-convert-a-list-to-string/

aa7616057f89dc78b7bbbd8f971953b7532b36d3
Pranav Dhumal <46155672+scorpiopd@users.noreply.github.com>
2021-04-21 15:19:27 +0530

    Update arraysorting.py

--- Files/arrays/arraysorting.py
+++ Files/arrays/arraysorting.py
@@ -1,7 +1,9 @@
-a="hello"
-
-b=len(a)-1
-v=a[1:b]
-
-print(a[-1]+v+a[0])
-
+number = [64, 25, 12, 22, 11, 1,2,44,3,122, 23, 34] 
+ 
+for i in range(len(number)): 
+    for j in range(i + 1, len(number)): 
+ 
+        if number[i] > number[j]: 
+           number[i], number[j] = number[j], number[i] 
+ 
+print (number) 

92b830ce0b50bdcc6dec44e5771a891c8ddbcdf1
Pranav Dhumal <46155672+scorpiopd@users.noreply.github.com>
2021-04-22 16:29:10 +0530

    Update 2.py

--- Files/Ch2/W3/W3 strings/2.py
+++ Files/Ch2/W3/W3 strings/2.py
@@ -12,3 +12,18 @@ print(d)
 
 
 #https://www.geeksforgeeks.org/python-frequency-of-each-character-in-string/
+
+
+
+#to remove duplicates
+a=[5,2,5,1,3,4,4]
+
+d={}
+
+for i in a:
+    if i in d:
+        d[i]=True
+        
+    else:
+        d[i]=False
+print(d.keys())
